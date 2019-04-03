3e7e0a43f78eb010dbc0b1edea399cd99861c329
minnecoder <sdparkin@gmail.com>
2019-04-03 16:38:44 -0500

    Working at MVP

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     module: {
       rules: [
@@ -6,8 +8,22 @@ module.exports = {
           exclude: /node_modules/,
           use: {
             loader: "babel-loader"
-          }
         }
+    },
+        {
+         test: /\.html$/,
+         use: 
+            {
+              loader: "html-loader"
+            }
+        }
+        
       ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: "./src/index.html",
+          filename: "./index.html"
+        })
+      ]
   };
